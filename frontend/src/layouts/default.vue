886835069302f6fd27320154db55b0232dd25f08
Beda Kosata <beda@bigdigdata.com>
2025-01-09 14:38:37 +0100

    only show admin stuff to admins

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -21,39 +21,41 @@
           title="Settings"
           to="/settings/"
         />
-        <v-list-subheader>Admin</v-list-subheader>
-        <v-list-item
-          prepend-icon="mdi-file-multiple"
-          subtitle="Current validations by all users"
-          title="All validations"
-          to="/validation/admin"
-        />
-        <v-list-item
-          prepend-icon="mdi-clock"
-          subtitle="Past validations by all users"
-          title="Validation history"
-          to="/history"
-        />
-        <v-list-item
-          prepend-icon="mdi-chart-bar"
-          subtitle="Basic stats about validations"
-          title="Stats"
-          to="/stats"
-        />
-        <v-list-item
-          prepend-icon="mdi-account-multiple"
-          title="Users"
-          subtitle="User management"
-          to="/admin/users"
-        />
+        <section v-if="user.has_admin_role">
+          <v-list-subheader>Admin</v-list-subheader>
+          <v-list-item
+            prepend-icon="mdi-file-multiple"
+            subtitle="Current validations by all users"
+            title="All validations"
+            to="/validation/admin"
+          />
+          <v-list-item
+            prepend-icon="mdi-clock"
+            subtitle="Past validations by all users"
+            title="Validation history"
+            to="/history"
+          />
+          <v-list-item
+            prepend-icon="mdi-chart-bar"
+            subtitle="Basic stats about validations"
+            title="Stats"
+            to="/stats"
+          />
+          <v-list-item
+            prepend-icon="mdi-account-multiple"
+            title="Users"
+            subtitle="User management"
+            to="/admin/users"
+          />
+        </section>
       </v-list>
 
       <template #append>
         <v-divider />
         <v-list>
           <v-list-item
-            :subtitle="store.user?.email"
-            :title="[store.user?.first_name, store.user?.last_name].join(' ')"
+            :subtitle="user?.email"
+            :title="[user?.first_name, user?.last_name].join(' ')"
           />
         </v-list>
         <v-btn
@@ -78,7 +80,7 @@
 <script setup lang="ts">
 import { logout } from "@/lib/http/auth"
 import { useAppStore } from "@/stores/app"
-const store = useAppStore()
+const { user } = useAppStore()
 
 const drawer: Ref<boolean | null | undefined> = ref(null)
 </script>
