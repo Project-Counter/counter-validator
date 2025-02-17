47ca9e375247d6e61fbd0cbcf068f5e577203772
Beda Kosata <beda@bigdigdata.com>
2025-02-17 11:13:47 +0100

    make silk optional using .env in devel settings

--- config/settings/devel.py
+++ config/settings/devel.py
@@ -5,13 +5,21 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3030"]
 
 # debug toolbar
-INSTALLED_APPS += ("debug_toolbar", "silk")  # noqa F405
+USE_SILK = config("USE_SILK", cast=bool, default=False)  # noqa F405, config comes from .base
+
+INSTALLED_APPS += ("debug_toolbar",)  # noqa F405
+if USE_SILK:
+    INSTALLED_APPS += ("silk",)  # noqa F405
+
+prepend_middlewares = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+if USE_SILK:
+    prepend_middlewares.append("silk.middleware.SilkyMiddleware")
+
 MIDDLEWARE = (
     MIDDLEWARE[:-1]  # noqa F405
-    + (
-        "silk.middleware.SilkyMiddleware",
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    )
+    + tuple(prepend_middlewares)
     + MIDDLEWARE[-1:]  # noqa F405
     + ("core.middleware.DebugSleepMiddleware",)
 )
