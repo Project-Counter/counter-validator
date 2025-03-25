b38446ee5aa0c4a4fd704b82abef8432d82e3b4c
Beda Kosata <beda@bigdigdata.com>
2025-03-25 18:23:35 +0100

    add vite preloadError handler to force page reload on issues

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,3 +18,10 @@ const app = createApp(App)
 registerPlugins(app)
 
 app.mount("#app")
+
+// This is a workaround for a Vite issue where the preload fails
+// after a new version of the app is deployed.
+window.addEventListener("vite:preloadError", (event) => {
+  console.error("Vite preload error", event)
+  window.location.reload()
+})
