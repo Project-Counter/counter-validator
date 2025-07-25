9a7b5f78ca539c965bec23fa58c49b8a1ac5a8bf
Beda Kosata <beda@bigdigdata.com>
2025-07-25 13:18:58 +0200

    feat: add tooltip for user email with copy functionality to list of all validations

--- frontend/src/components/UserName.vue
+++ frontend/src/components/UserName.vue
@@ -1,12 +1,34 @@
 <template>
-  <span v-if="user"
-    >{{ user.first_name || user.last_name ? `${user.first_name} ${user.last_name}` : user.email }}
-  </span>
-  <span v-else>-</span>
+  <v-tooltip v-if="user">
+    <template #activator="{ props }">
+      <span
+        v-bind="props"
+        class="cursor-pointer"
+        @click="copyEmail"
+      >
+        {{
+          user.first_name || user.last_name ? `${user.first_name} ${user.last_name}` : user.email
+        }}
+      </span>
+    </template>
+    <span>{{ user.email }}</span>
+  </v-tooltip>
 </template>
 
 <script setup lang="ts">
 import { User } from "@/lib/definitions/api"
+import { useAppStore } from "@/stores/app"
 
-defineProps<{ user?: User }>()
+const props = defineProps<{ user?: User }>()
+const store = useAppStore()
+
+function copyEmail() {
+  if (props.user?.email) {
+    navigator.clipboard.writeText(props.user.email)
+    store.displayNotification({
+      message: "Email copied to clipboard",
+      type: "success",
+    })
+  }
+}
 </script>
